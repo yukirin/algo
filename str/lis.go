aa5f07147e08dab1f8e8a8f589c51135f487a6bb
yukirin <standupdown@gmail.com>
2016-02-23 21:15:45 +0900

    Modify LIS

--- str/lis.go
+++ str/lis.go
@@ -3,25 +3,18 @@ package str
 import "sort"
 
 // LIS is longest increasing subsequence
-func LIS(seq []int) []int {
+func LIS(seq []int) int {
 	inf := 1<<63 - 1
-	max := 0
 
-	ret := make([]int, len(seq))
 	dp := make([]int, len(seq))
 	for i := range dp {
 		dp[i] = inf
-		ret[i] = inf
 	}
 
 	for _, n := range seq {
 		i := sort.SearchInts(dp, n)
 		dp[i] = n
-		if i+1 > max {
-			max = i + 1
-			copy(ret, dp[:i+1])
-		}
 	}
 
-	return ret[:max]
+	return sort.SearchInts(dp, inf)
 }
