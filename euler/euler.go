3c599bb9319336cca1aeca7dacb101ce2ad98981
yukirin <standupdown@gmail.com>
2016-03-07 00:02:13 +0900

    Add euler tour

---
+++ euler/euler.go
@@ -0,0 +1,24 @@
+package euler
+
+// Tour is euler tour ( [begin[i]:end[i]] )
+func Tour(root int, adjL [][]int) ([]int, []int, []int) {
+	tour := make([]int, 0, 2*len(adjL)-1)
+	begin, end := make([]int, len(adjL)), make([]int, len(adjL))
+	c := 0
+
+	var f func(int)
+	f = func(i int) {
+		begin[i] = c
+		tour = append(tour, i)
+		c++
+		for _, v := range adjL[i] {
+			f(v)
+			tour = append(tour, i)
+			c++
+		}
+		end[i] = c
+	}
+
+	f(root)
+	return tour, begin, end
+}
