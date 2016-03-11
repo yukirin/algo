64ba2d18fb2e5e03182202b0e202c9c2853e1964
yukirin <standupdown@gmail.com>
2016-03-11 20:12:06 +0900

    Add lazy segment tree

---
+++ segment/lasy.go
@@ -0,0 +1,69 @@
+package segment
+
+import "math"
+
+// NewLazy return the initialized lazy segment tree (n == 2^p)
+func NewLazy(n int) ([]int64, []int64) {
+	t := New(n)
+	return t, make([]int64, len(t))
+}
+
+// NewLazyS returns the initialization lazy segment tree value of the slice
+func NewLazyS(inits []int64) ([]int64, []int64) {
+	t := NewS(inits)
+	return t, make([]int64, len(t))
+}
+
+func lazyEval(i int, t, lazy []int64) {
+	t[i] += lazy[i]
+	if i < (len(t)+1)/2-1 {
+		lazy[i*2+1] += lazy[i]
+		lazy[i*2+2] += lazy[i]
+	}
+	lazy[i] = 0
+}
+
+// LazyUpdate update the lazy segment tree
+func LazyUpdate(a, b, i, l, r int, x int64, t, lazy []int64) {
+	if r <= a || b <= l {
+		return
+	}
+
+	if a <= l && r <= b {
+		lazy[i] += x
+		lazyEval(i, t, lazy)
+		return
+	}
+
+	lazyEval(i, t, lazy)
+	LazyUpdate(a, b, i*2+1, l, (l+r)/2, x, t, lazy)
+	LazyUpdate(a, b, i*2+2, (l+r)/2, r, x, t, lazy)
+
+	if t[i*2+1] < t[i*2+2] {
+		t[i] = t[i*2+1]
+	} else {
+		t[i] = t[i*2+2]
+	}
+	return
+}
+
+// LazyQuery to run a query in the interval
+func LazyQuery(a, b, i, l, r int, t, lazy []int64) int64 {
+	lazyEval(i, t, lazy)
+
+	if r <= a || b <= l {
+		return math.MaxInt64
+	}
+
+	if a <= l && r <= b {
+		return t[i]
+	}
+
+	vl := LazyQuery(a, b, i*2+1, l, (l+r)/2, t, lazy)
+	vr := LazyQuery(a, b, i*2+2, (l+r)/2, r, t, lazy)
+
+	if vl < vr {
+		return vl
+	}
+	return vr
+}
